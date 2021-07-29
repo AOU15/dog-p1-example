8d23dd97fc21f5907c386b961924d1d92c0703a0
Ada U <adau@Adas-MBP.lan1>
2021-07-29 14:17:15 -0400

    pseudocode

---
+++ app.js
@@ -0,0 +1,22 @@
+// Create Dynamic dropdown menu
+  // Make a request and convert response to dropdown menu "https://dog.ceo/api/breeds/list/all"
+
+
+// create form option tags from request
+
+
+//get value of option tag from the form select tag
+
+
+// set up eventHandler
+
+
+// make a request for the selected breed's image
+// random breed image URL "https://dog.ceo/api/breed/${breed}/images/random"
+
+
+//create img tag, assign src attribute, and append image 
+
+
+
+// remove previous image on new search
